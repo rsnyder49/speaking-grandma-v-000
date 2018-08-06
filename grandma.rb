2bfd3e3c763fdea0373709c8f4f80c6c196d6433
Robert Snyder <r.snyder49@gmail.com>
2018-08-06 19:42:05 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,10 +13,10 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(string)
-  if string === string.upcase 
-    puts "NO, NOT SINCE 1938!"
-  elsif string === "I LOVE YOU GRANDMA!"
+  if string === "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
+  elsif  string === string.upcase
+    puts "NO, NOT SINCE 1938!"
   else 
     puts "HUH?! SPEAK UP, SONNY!" 
   end 
